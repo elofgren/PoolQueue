330976f2f6d49bec8ba01ad0fc28a139587717db
Eric Lofgren <lofgrene@vbi.vt.edu>
2013-12-04 18:04:11 -0500

    Trajectory input prep
    Rewording some variables - outcomes data will only require one data
    pull, not two. Trying to implement with fixed time intervals, but this
    may require a loop.

--- cdiff_analysis.py
+++ cdiff_analysis.py
@@ -29,22 +29,24 @@ header = "Incident, Recur"
 CDIPool = stochpy.SSA()
 CDIPool.Model(File='CDIpool.psc', dir=os.getcwd())
 PoolOutcomes = numpy.zeros([n_runs,2])
+PoolTrajectories = numpy.zeros([8760,n_runs])
 
 def CDIPoolRun(model,iteration):
     model.Endtime(end_time)
     model.DoStochSim()
-    cumulative_outcomes = model.data_stochsim.getSpecies()
-    Incident = cumulative_outcomes[-1,9]
-    Recur = cumulative_outcomes[-1,13]
+    outcomes = model.data_stochsim.getSpecies()
+    Incident = outcomes[-1,9]
+    Recur = outcomes[-1,13]
     PoolOutcomes[iteration,0] = Incident
     PoolOutcomes[iteration,1] = Recur
-    
+
 for i in range(0,n_runs):
     print "CDI Pool Iteration %i of %i" % (i+1,n_runs)
     CDIPoolRun(CDIPool,i)
 
 print PoolOutcomes
 
+print PoolTrajectories
 
     
 
