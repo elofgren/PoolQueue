ea424f374423031be645ca0b2f9befd1f5d80d0c
elofgren <lofgrene@vbi.vt.edu>
2014-01-13 02:01:27 -0500

    LV competition model code
    Implemented, but untested. PML model files still need fixing.

--- LV_analysis.py
+++ LV_analysis.py
@@ -51,75 +51,31 @@ numpy.savetxt('PoolTotal.csv',PoolTotal,delimiter=',',header="TotalN",comments='
 
 print "LV Pool Model - Runs Complete"
     
-
-
-
-
-
-
-
-
 #########################
-# Pool-based Entry/Exit #
+# Queue-based Migration #
 #########################
 
-LVPool = stochpy.SSA()
-LVPool.Model(File='LVpool.psc', dir=os.getcwd())
-PoolRabbits = numpy.empty([end_time,n_runs])
-PoolFoxes = numpy.empty([end_time,n_runs])
-PoolTotal = numpy.empty([end_time,n_runs])
-
-# Note - specific model outcome array references will change for each model file
-def LVPoolRun(model,iteration):
-    model.Endtime(end_time)
-    #model.Method('Direct')
-    model.DoStochSim()
-    model.GetRegularGrid(npoints=end_time*10)
-    population = model.data_stochsim_grid.species
-    for t in range(0,end_time):
-    	PoolRabbits[t,iteration] = population[0][0][t]
-    	PoolFoxes[t,iteration] = population[1][0][t]
-    	PoolTotal[t,iteration] = PoolRabbits[t,iteration] + PoolFoxes[t,iteration]
-
-for i in range(0,n_runs):
-    print "LV Pool Iteration %i of %i" % (i+1,n_runs)
-    LVPoolRun(LVPool,i)
-    
-numpy.savetxt('PoolRabbits.csv',PoolRabbits,delimiter=',',header="Rabbits",comments='')
-numpy.savetxt('PoolFoxes.csv',PoolFoxes,delimiter=',',header="Foxes",comments='')
-numpy.savetxt('PoolWholeLV.csv',PoolTotal,delimiter=',',header="N",comments='')
-
-print "LV Pool Model - Runs Complete"
-
-##########################
-# Queue-based Entry/Exit #
-##########################
-
-LVQueue = stochpy.SSA()
-LVQueue.Model(File='LVqueue.psc', dir=os.getcwd())
-QueueRabbits = numpy.empty([end_time,n_runs])
-QueueFoxes = numpy.empty([end_time,n_runs])
+LVqueue = stochpy.SSA()
+LVqueue.Model(File='LVcomp_queue.psc', dir = os.getcwd())
+QueueInvader = numpy.empty([end_time,n_runs])
+QueueNative = numpy.empty([end_time,n_runs])
 QueueTotal = numpy.empty([end_time,n_runs])
 
-# Note - specific model outcome array references will change for each model file
 def LVQueueRun(model,iteration):
     model.Endtime(end_time)
-    #model.Method('Direct')
     model.DoStochSim()
     model.GetRegularGrid(npoints=end_time*10)
-    population = model.data_stochsim_grid.species
+    population = model.data_stohsim_grid.species
     for t in range(0,end_time):
-    	QueueRabbits[t,iteration] = population[0][0][t]
-    	QueueFoxes[t,iteration] = population[1][0][t]
-    	QueueTotal[t,iteration] = PoolRabbits[t,iteration] + PoolFoxes[t,iteration]
+        QueueInvader[t,iteration] = population[0][0][t]
+        QueueNative[t,iteration] = population[1][0][t]
+        QueueTotal[t,iteration] = QueueInvader[t,iteration]+QueueNative[t,iteration]
 
 for i in range(0,n_runs):
     print "LV Queue Iteration %i of %i" % (i+1,n_runs)
-    LVQueueRun(LVQueue,i)
-    
-numpy.savetxt('QueueRabbits.csv',QueueRabbits,delimiter=',',header="Rabbits",comments='')
-numpy.savetxt('QueueFoxes.csv',QueueFoxes,delimiter=',',header="Foxes",comments='')
-numpy.savetxt('QueueWholeLV.csv',QueueTotal,delimiter=',',header="N",comments='')
+
+numpy.savetxt('QueueInvader.csv',QueueInvader,delimiter=',',header="Invader",comments='')
+numpy.savetxt('QueueNative.csv',QueueNative,delimiter=',',header="Native",comments='')
+numpy.savetxt('QueueTotal.csv',QueueTotal,delimiter=',',header="TotalN",comments='')
 
 print "LV Queue Model - Runs Complete"
-
