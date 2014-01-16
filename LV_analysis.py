86b842d37cca01b353d95d6d550d64ad7fca395c
Eric Lofgren <lofgrene@vbi.vt.edu>
2014-01-16 18:16:22 -0500

    LV model run updates
    Updating with new file names and the like.

--- LV_analysis.py
+++ LV_analysis.py
@@ -11,71 +11,71 @@ import pylab as pl
 import numpy as numpy
 
 # Pull down most recent files from internet
-LVPoolDownload = ph.NetDrop('https://raw.github.com/elofgren/PML/master/Ecology/LVcomp.psc',
-'LVcomp_pool.psc')
+LVPoolDownload = ph.NetDrop(
+'https://raw.github.com/elofgren/PML/master/Ecology/LVpred_secondary.psc','LVpool.psc')
 
-LVQueueDownload = ph.NetDrop('https://raw.github.com/elofgren/PML/master/Ecology/LVcomp_queue.psc'
-,'LVcomp_queue.psc')	
+LVQueueDownload = ph.NetDrop
+('https://raw.github.com/elofgren/PML/master/Ecology/LVpred_secondary_queue.psc','LVqueue.psc')	
 
 # General simulation parameters
 start_time = 0.0
 end_time = 100
 n_runs = 50
 
-########################
-# Pool-based Migration #
-########################
+######################################
+# Pool-based Birth/Death of Predator #
+######################################
 
 LVpool = stochpy.SSA()
-LVpool.Model(File='LVcomp_pool.psc', dir = os.getcwd())
-PoolInvader = numpy.empty([end_time,n_runs])
-PoolNative = numpy.empty([end_time,n_runs])
+LVpool.Model(File='LVpool.psc', dir = os.getcwd())
+PoolPrey = numpy.empty([end_time,n_runs])
+PoolPredator = numpy.empty([end_time,n_runs])
 PoolTotal = numpy.empty([end_time,n_runs])
 
 def LVPoolRun(model,iteration):
     model.Endtime(end_time)
     model.DoStochSim()
     model.GetRegularGrid(npoints=end_time*10)
-    population = model.data_stohsim_grid.species
+    population = model.data_stochsim_grid.species
     for t in range(0,end_time):
-        PoolInvader[t,iteration] = population[0][0][t]
-        PoolNative[t,iteration] = population[1][0][t]
-        PoolTotal[t,iteration] = PoolInvader[t,iteration]+PoolNative[t,iteration]
+        PoolPrey[t,iteration] = population[0][0][t]
+        PoolPredator[t,iteration] = population[1][0][t]
+        PoolTotal[t,iteration] = PoolPrey[t,iteration]+PoolPredator[t,iteration]
 
 for i in range(0,n_runs):
     print "LV Pool Iteration %i of %i" % (i+1,n_runs)
 
-numpy.savetxt('PoolInvader.csv',PoolInvader,delimiter=',',header="Invader",comments='')
-numpy.savetxt('PoolNative.csv',PoolNative,delimiter=',',header="Native",comments='')
+numpy.savetxt('PoolPrey.csv',PoolPrey,delimiter=',',header="Prey",comments='')
+numpy.savetxt('PoolPredator.csv',PoolPredator,delimiter=',',header="Pred",comments='')
 numpy.savetxt('PoolTotal.csv',PoolTotal,delimiter=',',header="TotalN",comments='')
 
 print "LV Pool Model - Runs Complete"
     
-#########################
-# Queue-based Migration #
-#########################
+#######################################
+# Queue-based Birth/Death of Predator #
+#######################################
 
 LVqueue = stochpy.SSA()
-LVqueue.Model(File='LVcomp_queue.psc', dir = os.getcwd())
-QueueInvader = numpy.empty([end_time,n_runs])
-QueueNative = numpy.empty([end_time,n_runs])
+LVqueue.Model(File='LVqueue.psc', dir = os.getcwd())
+QueuePrey = numpy.empty([end_time,n_runs])
+QueuePredator = numpy.empty([end_time,n_runs])
 QueueTotal = numpy.empty([end_time,n_runs])
 
 def LVQueueRun(model,iteration):
     model.Endtime(end_time)
     model.DoStochSim()
     model.GetRegularGrid(npoints=end_time*10)
-    population = model.data_stohsim_grid.species
+    population = model.data_stochsim_grid.species
     for t in range(0,end_time):
-        QueueInvader[t,iteration] = population[0][0][t]
-        QueueNative[t,iteration] = population[1][0][t]
-        QueueTotal[t,iteration] = QueueInvader[t,iteration]+QueueNative[t,iteration]
+        QueuePrey[t,iteration] = population[0][0][t]
+        QueuePredator[t,iteration] = population[1][0][t]
+        QueueTotal[t,iteration] = QueuePrey[t,iteration]+QueuePredator[t,iteration]
 
 for i in range(0,n_runs):
     print "LV Queue Iteration %i of %i" % (i+1,n_runs)
 
-numpy.savetxt('QueueInvader.csv',QueueInvader,delimiter=',',header="Invader",comments='')
-numpy.savetxt('QueueNative.csv',QueueNative,delimiter=',',header="Native",comments='')
+numpy.savetxt('QueuePrey.csv',QueuePrey,delimiter=',',header="Prey",comments='')
+numpy.savetxt('QueuePredator.csv',QueuePredator,delimiter=',',header="Predator",comments='')
 numpy.savetxt('QueueTotal.csv',QueueTotal,delimiter=',',header="TotalN",comments='')
 
 print "LV Queue Model - Runs Complete"
