00542d05e27b4a47b12909d5f611308028aa3ecd
elofgren <lofgrene@vbi.vt.edu>
2014-01-20 04:42:24 -0500

    Data analysis upload
    Uploading initial R script for ecology model analysis, adding new
    output data sets for the ecology model.

--- Analysis.R
+++ Analysis.R
@@ -8,6 +8,42 @@ library(gridExtra)
 library(plyr)
 
 ### Analysis of Ecology Results ###
+## C- and N- Extinction Probability
+PoolExtinction <- as.data.frame(read.csv("~/Documents/Code/PoolQueue/LVpoolExtinction.csv"))
+QueueExtinction <- as.data.frame(read.csv("~/Documents/Code/PoolQueue/LVqueueExtinction.csv"))
 
-## Pool-based Predation ##
-PoolFoxes <- read.csv("PoolFoxes.csv",header=FALSE,skip=1)
+# Prey
+pool_trials <- nrow(PoolExtinction)
+queue_trials <- nrow(QueueExtinction)
+
+extinctprey_pool <- sum(PoolExtinction$Prey)
+extinctprey_queue <- sum(QueueExtinction$Prey)
+
+PreyExtinctTest <- prop.test(x = c(extinctprey_pool, extinctprey_queue), n = c(pool_trials,queue_trials))
+PreyExtinctDiff <- (extinctprey_pool/pool_trials) - (extinctprey_queue/queue_trials)
+PreyExtinctCI <- PreyExtinctTest$conf.int
+PreyExtinctP <- PreyExtinctTest$p.value
+
+# Predators
+extinctpred_pool <- sum(PoolExtinction$Predator)
+extinctpred_queue <- sum(QueueExtinction$Predator)
+
+PredExtinctTest <- prop.test(x = c(extinctpred_pool, extinctpred_queue), n = c(pool_trials,queue_trials))
+PredExtinctDiff <- (extinctpred_pool/pool_trials) - (extinctpred_queue/queue_trials)
+PredExtinctCI <- PredExtinctTest$conf.int
+PredExtinctP <- PredExtinctTest$p.value
+
+# N-extinction
+extinctN_pool <- sum(PoolExtinction$N)
+extinctN_queue <- sum(QueueExtinction$N)
+
+NExtinctTest <- prop.test(x = c(extinctN_pool, extinctN_queue), n = c(pool_trials,queue_trials))
+NExtinctDiff <- (extinctN_pool/pool_trials) - (extinctN_queue/queue_trials)
+NExtinctCI <- NExtinctTest$conf.int
+NExtinctP <- NExtinctTest$p.value
+
+## Mean and Median Species Populations
+
+
+
+
--- LV_analysis.py
+++ LV_analysis.py
@@ -30,6 +30,8 @@ PoolPrey = numpy.empty([end_time,n_runs])
 PoolPredator = numpy.empty([end_time,n_runs])
 PoolTotal = numpy.empty([end_time,n_runs])
 PoolExtinction = numpy.empty([n_runs,3])
+PoolMedian = numpy.empty([n_runs,3])
+PoolMean = numpy.empty([n_runs,3])
 
 def LVPoolRun(model,iteration):
     model.Endtime(end_time)
@@ -40,18 +42,25 @@ def LVPoolRun(model,iteration):
         PoolPrey[t,iteration] = population[0][0][t]
         PoolPredator[t,iteration] = population[1][0][t]
         PoolTotal[t,iteration] = PoolPrey[t,iteration]+PoolPredator[t,iteration]
-    if population[0][0][-1] == 0:
-        PoolExtinction[iteration,0] = 1
+    if PoolPrey[-1,iteration] == 0:
+    	PoolExtinction[iteration,0] = 1
     else:
-        PoolExtinction[iteration,0] = 0
-    if population[1][0][-1] ==0:
+    	PoolExtinction[iteration,0] = 0
+    if PoolPredator[-1,iteration] == 0:
     	PoolExtinction[iteration,1] = 1
     else:
     	PoolExtinction[iteration,1] = 0
-    if population[0][0][-1] + population[1][0][-1] == 0:
+    if PoolTotal[-1,iteration] == 0:
     	PoolExtinction[iteration,2] = 1
     else:
     	PoolExtinction[iteration,2] = 0
+    PoolMedian[iteration,0] = numpy.median(PoolPrey[:,iteration])
+    PoolMedian[iteration,1] = numpy.median(PoolPredator[:,iteration])
+    PoolMedian[iteration,2] = numpy.median(PoolTotal[:,iteration])
+    PoolMean[iteration,0] = numpy.mean(PoolPrey[:,iteration])
+    PoolMean[iteration,1] = numpy.mean(PoolPredator[:,iteration])
+    PoolMean[iteration,2] = numpy.mean(PoolTotal[:,iteration])
+    
 
 for i in range(0,n_runs):
 	LVPoolRun(LVpool,i)
@@ -61,6 +70,8 @@ numpy.savetxt('PoolPrey.csv',PoolPrey,delimiter=',',header="",comments='')
 numpy.savetxt('PoolPredator.csv',PoolPredator,delimiter=',',header="",comments='')
 numpy.savetxt('PoolTotal.csv',PoolTotal,delimiter=',',header="",comments='')
 numpy.savetxt('LVpoolExtinction.csv',PoolExtinction,delimiter=',',header="Prey,Predator,N",comments='')
+numpy.savetxt('LVpoolMedian.csv',PoolMedian,delimiter=',', header="Prey,Predator,N",comments='')
+numpy.savetxt('LVpoolMean.csv',PoolMean,delimiter=',',header="Prey,Predator,N",comments='')
 
 print "LV Pool Model - Runs Complete"
     
@@ -74,6 +85,8 @@ QueuePrey = numpy.empty([end_time,n_runs])
 QueuePredator = numpy.empty([end_time,n_runs])
 QueueTotal = numpy.empty([end_time,n_runs])
 QueueExtinction = numpy.empty([n_runs,3])
+QueueMedian = numpy.empty([n_runs,3])
+QueueMean = numpy.empty([n_runs,3])
 
 
 def LVQueueRun(model,iteration):
@@ -85,18 +98,24 @@ def LVQueueRun(model,iteration):
         QueuePrey[t,iteration] = population[0][0][t]
         QueuePredator[t,iteration] = population[1][0][t]
         QueueTotal[t,iteration] = QueuePrey[t,iteration]+QueuePredator[t,iteration]
-	if population[0][0][-1] == 0:
+	if QueuePrey[-1,iteration] == 0:
 		QueueExtinction[iteration,0] = 1
-    else:
-        QueueExtinction[iteration,0] = 0
-    if population[1][0][-1] ==0:
-    	QueueExtinction[iteration,1] = 1
-    else:
-    	QueueExtinction[iteration,1] = 0
-    if population[0][0][-1] + population[1][0][-1] == 0:
-    	QueueExtinction[iteration,2] = 1
-    else:
-    	QueueExtinction[iteration,2] = 0
+	else:
+		QueueExtinction[iteration,0] = 0
+	if QueuePredator[-1,iteration] == 0:
+		QueueExtinction[iteration,1] = 1
+	else:
+		QueueExtinction[iteration,1] = 0
+	if QueueTotal[-1,iteration] == 0:
+		QueueExtinction[iteration,2] = 1
+	else:
+		QueueExtinction[iteration,2] = 0
+	QueueMedian[iteration,0] = numpy.median(QueuePrey[:,iteration])
+    QueueMedian[iteration,1] = numpy.median(QueuePredator[:,iteration])
+    QueueMedian[iteration,2] = numpy.median(QueueTotal[:,iteration])
+    QueueMean[iteration,0] = numpy.mean(QueuePrey[:,iteration])
+    QueueMean[iteration,1] = numpy.mean(QueuePredator[:,iteration])
+    QueueMean[iteration,2] = numpy.mean(QueueTotal[:,iteration])
 
 for i in range(0,n_runs):
 	LVQueueRun(LVqueue,i)
@@ -106,6 +125,7 @@ numpy.savetxt('QueuePrey.csv',QueuePrey,delimiter=',',header="Prey",comments='')
 numpy.savetxt('QueuePredator.csv',QueuePredator,delimiter=',',header="Predator",comments='')
 numpy.savetxt('QueueTotal.csv',QueueTotal,delimiter=',',header="TotalN",comments='')
 numpy.savetxt('LVqueueExtinction.csv',QueueExtinction,delimiter=',',header="Prey,Predator,N",comments='')
-
+numpy.savetxt('LVqueueMedian.csv',QueueMedian,delimiter=',', header="Prey,Predator,N",comments='')
+numpy.savetxt('LVqueueMean.csv',QueueMean,delimiter=',',header="Prey,Predator,N",comments='')
 
 print "LV Queue Model - Runs Complete"
